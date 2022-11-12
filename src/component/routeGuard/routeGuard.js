516025a76a53e6478f7c82c5cc703d74dfdabd88
Ivan Ariel Almendra <almendraivan210814@gmail.com>
2022-11-12 14:00:41 -0300

    agregando para subirlo a EC2

---
+++ src/component/routeGuard/routeGuard.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+import {Route,Routes} from 'react-router-dom'
+import { Error } from '../errorLogin/error'
+
+export const RouteGuard = ({element}) => {
+
+    const [flag,setFlag] = useState(false)
+
+    const hasJWT = () => {
+        localStorage.getItem('token')
+            ? setFlag(true)
+            : setFlag(false)
+    }
+
+    const redireccion = () =>{
+        window.location.href='/!#/login'
+    }
+
+    console.log(flag)
+   // { flag ?  <Route path='/conexion' element={element} />:<Route path='/error'  element={<Error/>}/> }
+    return (
+        <Routes>
+            { flag ?  <Route path='/conexion' element={element} />:redireccion() }
+        </Routes>
+    )
+}
